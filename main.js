b2021d31288ec56cfca154baef50e220991abfbc
sahilthakur07 <90078299+sahilthakur07@users.noreply.github.com>
2021-11-14 13:36:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function openNav() {
+  document.getElementById("mySidebar").style.width = "400px";
+  document.getElementById("main").style.marginLeft = "400px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
